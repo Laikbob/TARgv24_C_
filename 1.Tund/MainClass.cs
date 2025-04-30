43f592ab6f26fd23b876a0510e74015b68759b75
Andrei Lebedev <andreilebedev321@gmail.com>
2025-04-30 17:31:22 +0300

    Commit Main

--- 1.Tund/MainClass.cs
+++ 1.Tund/MainClass.cs
@@ -20,108 +20,221 @@ namespace TARgv24_C_
             Console.WriteLine("Tere! Hea Päeva!");
 
             //// 1.osa Andmetüüpd, If, Case, Random, Alamfuktsioonid
-            //int a = 0;
-            //string tekst = "Python";
-            //char taht = 'A';
-            //double arv = 5.544454564;
-            //float arv1 = 2;
+            int a = 0;
+            string tekst = "Python";
+            char taht = 'A';
+            double arv = 5.45435333353;
+            float arv1 = 2;
+            Console.Write("Mis on sinu nimi? ");
+            tekst = Console.ReadLine();
+            Console.WriteLine("Tere!\n" + tekst);
+            Console.WriteLine("\nTere {0}!", tekst);
 
-            //Console.Write("Mis on sinu nimi? ");
-            //tekst = Console.ReadLine();
-            //Console.WriteLine("Tere!\n" + tekst);
-            //Console.WriteLine("Tere!\n{0}", tekst);
+            // II. osa C# Valikute konstruktsionid
+            if (tekst.ToLower() == "juku")
+            {
+                Console.WriteLine("Lähme kinno!");
+                try
+                {
+                    Console.WriteLine("{0}\n Kui vana sa oled?", tekst);
+                    int vanus = int.Parse(Console.ReadLine());
+                    string pilet_vastus = FunktsioonideClass_1_2osa.VanusePilet(vanus);
+                    Console.WriteLine(pilet_vastus);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Olen hõivatud");
+            }
 
-            //if (tekst.ToLower() == "juku")
-            //{
-            //    Console.WriteLine("Lahme kinno!");
-            //    try
-            //    {
-            //        Console.WriteLine("{0}\nKui vana sa oled?", tekst);
-            //        int vanus = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arv 2: ");
+            int arv2 = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+            arv1 = FunktsioonideClass_1_2osa.Kalkulaator(a, arv2);
+            Console.WriteLine(arv1);
 
-            //        if (vanus <= 0 || vanus > 100)
-            //        {
-            //            Console.WriteLine("Viga!");
-            //        }
-            //        else if (vanus <= 6)
-            //        {
-            //        }
-            //        else if (vanus <= 15)
-            //        {
-            //            Console.WriteLine("Lastepilet");
-            //        }
-            //        else if (vanus <= 65)
-            //        {
-            //            Console.WriteLine("Standartpilet");
-            //        }
-            //        else if (vanus <= 100)
-            //        {
-            //            Console.WriteLine("Vanapilet");
-            //        }
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.WriteLine(e.Message);
-            //    }
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Olen hõivatud!");
-            //}
+            Console.WriteLine("Switch'i kasutamine");
+            Random rnd = new Random();
+            a = rnd.Next(1, 7);
+            Console.WriteLine(a);
 
-            //Random rnd = new Random();
+            tekst = FunktsioonideClass_1_2osa.switchKasuta(a);
+            Console.WriteLine(tekst);
 
-            //Console.WriteLine("Arv 2: ");
-            //int arv2 = int.Parse(Console.ReadLine());
-
-            //arv1 = FunktsioonideClass.Kalkulaator(a, arv2);
-            //Console.WriteLine(arv1);
-
-            //a = rnd.Next();
-
-            ////Task nädal
-            //Console.WriteLine("Switch'i kasutamine");
-            //a = rnd.Next(1, 7);
-            //Console.WriteLine(a);
-
-            //tekst = FunktsioonideClass.switcKasuta(a);
-            //Console.WriteLine(tekst);
-
-            //Console.ReadKey();
-
-            //Task1  naber
-            //Console.Write("Sisesta esimene inimese nimi: ");
-            //string nimi1 =Console.ReadLine();
-            //Console.Write("Sisesta teine inimese nimi: ");
-            //string nimi2 = Console.ReadLine();
-            //string neigbor = FunktsioonideClass.küsiNaber(nimi1, nimi2);
-            //Console.WriteLine(neigbor);
-
-            ////Task 2 pikkus
-            //Console.Write("Sisesta toa pikkus (meetrites): ");
-            //string pikkus = Console.ReadLine();
-            //Console.Write("Sisesta toa laius (meetrites): ");
-            //string toa= Console.ReadLine();
+            Console.ReadKey();
 
 
+            // Küsi kahe inimese nimed ning teata, et nad on täna pinginaabrid
+            Console.Write("Sisesta esimene nimi:");
+            string nimi1 = Console.ReadLine();
+            Console.Write("Sisesta teine nimi:");
+            string nimi2 = Console.ReadLine();
+            string nimi_vastus = FunktsioonideClass_1_2osa.Pinginaabrid(nimi1, nimi2);
+            Console.WriteLine(nimi_vastus);
 
-            //Task 3 soodus
-            Console.Write("Sisesta toote hind pärast 30% soodustust: ");
-            float s = float.Parse(Console.ReadLine());
-            float soodust = FunktsioonideClass.soodus(s);
-            Console.WriteLine(soodust);
 
-            //Task 4 temp
-            Console.Write("Kui palju temperatuur on õues");
-            int t =int.Parse(Console.ReadLine());
-            FunktsioonideClass.temp(t);
+            // Küsi ristkülikukujulise toa seinte pikkused ning arvuta põranda pindala.
+            // Küsi kasutajalt remondi tegemise soov, kui ta on positiivne, siis küsi kui palju maksab ruutmeeter ja leia põranda vahetamise hind
+            double c = 0, d = 0;
+            try
+            {
+                Console.Write("Sisestage esimese seina pikkus:");
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e);
+            }
 
-            //Task 5 pikkus
-            Console.Write("Mis pikkus teil on ?");
-            int p = int.Parse(Console.ReadLine());
-                
+            try
+            {
+                Console.Write("Sisestage teise seina pikkus:");
+                d = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
+            double vastus = FunktsioonideClass_1_2osa.Korrutamine(c, d);
+            Console.WriteLine("Põranda pindala on " + vastus);
+            Console.Write("Kas soovite remodi teha?");
+            string vastus2 = Console.ReadLine();
+            if (vastus2.ToLower() == "jah")
+            {
+                try
+                {
+                    Console.Write("Sisestage kui palju maksab ruutmeeter:");
+                    double hind = Convert.ToDouble(Console.ReadLine());
+                    double vastus3 = FunktsioonideClass_1_2osa.Korrutamine(hind, vastus);
+                    Console.WriteLine("Põranda vahetamise hind on " + vastus3);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aitäh! Head aega!");
+            }
+
+
+            // Leia 30% hinnasoodustusega hinna põhjal alghind
+            try
+            {
+                Console.Write("Sisestage alghind: ");
+                double alghind = Convert.ToDouble(Console.ReadLine());
+                vastus = FunktsioonideClass_1_2osa.Hinnasoodustus(alghind);
+                Console.WriteLine("Soodustusega 30% lõpphind on " + vastus);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            // Küsi temperatuur ning teata, kas see on üle kaheksateistkümne kraadi (soovitav toasoojus talvel)
+            try
+            {
+                Console.Write("Sisestage toa temperatuur: ");
+                int temp = int.Parse(Console.ReadLine());
+                vastus2 = FunktsioonideClass_1_2osa.Temperatuur(temp);
+                Console.WriteLine(vastus2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            // Küsi inimese pikkus ning teata, kas ta on lühike, keskmine või pikk (piirid pane ise paika)
+            int pikkus = 0;
+            try
+            {
+                Console.Write("Sisestage oma pikkus: ");
+                pikkus = int.Parse(Console.ReadLine());
+                vastus2 = FunktsioonideClass_1_2osa.Pikkus(pikkus);
+                Console.WriteLine(vastus2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            // Küsi inimeselt pikkus ja sugu ning teata, kas ta on lühike, keskmine või pikk (mitu tingimusplokki võib olla üksteise sees).
+            try
+            {
+                Console.Write("Sisestage oma pikkus: ");
+                pikkus = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.Write("Sisestage oma sugu: ");
+            string sugu = Console.ReadLine();
+            vastus2 = FunktsioonideClass_1_2osa.PikkusSugu(pikkus, sugu);
+            Console.WriteLine(vastus2);
+
+
+            // Küsi inimeselt poes eraldi kas ta soovib osta piima, saia, leiba.
+            // Löö hinnad kokku ning teata, mis kõik ostetud kraam maksma läheb.
+            Console.Write("Kas te soovite osta piima? (jah/ei): ");
+            string piim_vastus = Console.ReadLine();
+            int piim_kogus = 0;
+            if (piim_vastus.ToLower() == "jah")
+            {
+                try
+                {
+                    Console.Write("Kui palju piima soovite osta? ");
+                    piim_kogus = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.Write("Kas te soovite osta saia?(jah/ei): ");
+            string sai_vastus = Console.ReadLine();
+            int sai_kogus = 0;
+            if (sai_vastus.ToLower() == "jah")
+            {
+                try
+                {
+                    Console.Write("Kui palju saia soovite osta? ");
+                    sai_kogus = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.Write("Kas te soovite osta leiba?(jah/ei): ");
+            string leib_vastus = Console.ReadLine();
+            int leib_kogus = 0;
+            if (leib_vastus.ToLower() == "jah")
+            {
+                try
+                {
+                    Console.Write("Kui palju saia soovite osta? ");
+                    leib_kogus = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            string ostutsekk = FunktsioonideClass_1_2osa.Ostukorv(piim_kogus, sai_kogus, leib_kogus);
+            Console.WriteLine("\n" + ostutsekk);
         }
-
     }
 }
