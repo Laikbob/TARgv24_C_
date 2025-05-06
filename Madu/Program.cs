88a7c4aebaedb437d65ce7b95dfbd014b64ce2a1
Andrei Lebedev <andreilebedev321@gmail.com>
2025-05-06 20:12:08 +0300

    VertikalLine

--- Madu/Program.cs
+++ Madu/Program.cs
@@ -9,7 +9,7 @@ namespace TARgv24_C_.Madu
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
             Point p1 = new Point(1,3, '*');
             p1.Draw();
 
@@ -18,6 +18,24 @@ namespace TARgv24_C_.Madu
 
             HorisontalLine line = new HorisontalLine(5,10,8,'+');
             line.Drow();
+
+
+            Console.SetWindowSize(80, 25);
+
+
+            //Границы поля
+            HorisontalLine upLine= new HorisontalLine( 0, 78, 0, '=');
+            HorisontalLine downLine = new HorisontalLine(0, 78, 24, '=');
+            VertikalLine leftLine = new VertikalLine(0, 24, 0, '=');
+            VertikalLine rightLine = new VertikalLine(0,24, 78,'=');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            Point p = new Point(4,5,'*');
+            p.Draw();
+
 
             Console.ReadLine();
         }
