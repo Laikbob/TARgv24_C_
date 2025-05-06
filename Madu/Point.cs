b090344f37a0b6b372214344a7c3d00a49ce8161
Andrei Lebedev <andreilebedev321@gmail.com>
2025-05-06 15:29:53 +0300

    Konstruktor

--- Madu/Point.cs
+++ Madu/Point.cs
@@ -12,6 +12,19 @@ namespace TARgv24_C_.Madu
         public int y;
         public char sym;
 
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+            
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
